32e2166c7dade42826673666fe0129425ede106c
Benjamin <31835324+Fuma419@users.noreply.github.com>
2024-03-18 11:15:44 -0400

    making the api key publicly readable.
    exposing this api key was the only way I could find to make this work.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { KoiosProvider } from "@meshsdk/core";
 import { StakeButton, MeshBadge } from "@meshsdk/react";
 
 export default function Home() {
-  const blockchainProvider = new KoiosProvider('api', process.env.KOIOS_API_KEY);
+  const blockchainProvider = new KoiosProvider("api", process.env.NEXT_PUBLIC_KOIOS_API_KEY);
 
   return (
     <div className="container">
